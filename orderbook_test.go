e566a55deddf017da352b97382986052d3480f17
SUA <suaone01@gmail.com>
2026-02-16 14:07:37 +0700

    add github actions

---
+++ orderbook_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLimit(t *testing.T) {
+	l := NewLimit(10_000)
+	buyOrderA := NewOrder(true, 5)
+	buyOrderB := NewOrder(true, 2)
+	buyOrderC := NewOrder(true, 7)
+
+	l.AddOrder(buyOrderA)
+	l.AddOrder(buyOrderB)
+	l.AddOrder(buyOrderC)
+
+	l.DeleteOrder(buyOrderB)
+
+	fmt.Println(l)
+}
